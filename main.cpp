b5c5fa0f0b4aa7877941e68f8e3e3d19b0ab7e87
DanyaRBR <riokingv2@gmail.com>
2020-11-21 03:43:48 +0700

    cppFile3

--- main.cpp
+++ main.cpp
@@ -1,19 +1,30 @@
 ﻿#include <iostream>
 using namespace std;
 
-double my_pow(double number, unsigned int stepen) {
+double my_pow(double number, int stepen) {
     double result = 1;
-     for (int i = 0; i < stepen; i++) {
+    if (stepen < 0) {
+        stepen = -stepen;
+        for (int i = 0; i < stepen; i++) {
+            result *= number;
+        }
+
+        cout << 1 / result;
+    }
+    else {
+        for (int i = 0; i < stepen; i++) {
             result = result * number;
         }
         cout << "result = " << result;
         return result;
+    }
 }
+
 
 int main()
 {
     double number;
-    unsigned int stepen;
+    int stepen;
     cout << "Enter number";
     cin >> number;
     cout << "Enter stepen";
