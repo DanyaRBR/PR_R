d9f3ebe3c0af39484a920727aa27c1721124204e
DanyaRBR <riokingv2@gmail.com>
2020-11-21 02:49:55 +0700

    cppFile1

---
+++ main.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+double my_pow(double number, unsigned int stepen) {
+    double result = 1;
+     for (int i = 0; i < stepen; i++) {
+            result = result * number;
+        }
+        cout << "result = " << result;
+        return result;
+}
+
+int main()
+{
+    double number;
+    unsigned int stepen;
+    cout << "Enter number";
+    cin >> number;
+    cout << "Enter stepen";
+    cin >> stepen;
+
+    my_pow(number, stepen);
+}
